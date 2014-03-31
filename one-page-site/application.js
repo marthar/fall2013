d667081c4083cc31c415a8b3ae4c09213027235d
Julia Paranay <juliaparanay@Julia-Paranays-MacBook-Pro.local>
2014-03-31 17:52:39 -0400

    woo

--- one-page-site/application.js
+++ one-page-site/application.js
@@ -9,14 +9,80 @@ function showProduct(name){
 		$("#detail-image").attr("src",earring.image);
 		$("#detail-description").text(earring.description);
 		$("#detail-price").text("$" + earring.price);
+
+		$("#add-to-cart").click(function() {
+			var quantity=parseInt($("#detail-quantity").val())
+			addItem(name, quantity)
+
+		});
+}
+
+function addItem(name, quantity){
+	if(!cart[name]) {cart[name] = 0;}
+	cart[name] += 1*quantity;
+	updateCart()
+}
+
+
+var cart = {};
+
+function updateCart(){
+	var total = 0;
+
+	for(var itemName in cart) {
+		var earring= Earrings[itemName];
+		var quantity = cart[itemName];
+		var itemPrice= earring.price*quantity;
+		total += itemPrice;
+	}
+
+	$("#cart").text("Total Cost" + ":" + " " + "$" + total)
 }
 
 
 function hideProduct(){
 		$(".overlay").fadeOut("slow");
 		$(".details").fadeOut("slow");
+
+		$("#add-to-cart").off("click");
 	
 }
+
+
+
+    
+function checkOut() {
+
+  var stripeKey = 'pk_test_V0SJ6QOh3rXO9s6Ysw0eHzzE';
+
+  var description = $("#cart").text();
+  var amount = updateCart() * 100;
+
+  var handler = StripeCheckout.configure({
+    key: stripeKey,
+    image: 'http://www.astutegraphics.com/images/blog/tutorials/widthscribe_patterns_18_mar_2013/floral-seamless-pattern.png',
+    token: function(token, args) {
+      
+    }
+  });
+
+
+
+
+  handler.open({
+    name: 'Dangles & Bangles',
+    description: description,
+    amount: amount
+  });
+
+}
+    
+
+
+
+
+
+
 
 
 // When the page loads, add in our event handlers
@@ -31,5 +97,9 @@ $(function() {
 	$(".overlay").click(function() {
 		hideProduct();
 	});
+
+		$("#cart").click(function() {
+	checkOut();
+});
 
 });
